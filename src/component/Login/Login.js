dd878cd8e2acc96f250b5948ebdc08b8aea2d479
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2023-01-27 13:00:03 +0600

    logo issue solve

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -5,7 +5,10 @@ const Login = () => {
   return (
     <Container>
       <Content>
-        <BgImg/>
+        <CTA>
+          <CTALogoOne src="/images/rtaImage-removebg-preview.png" alt="logo" />
+        </CTA>
+        <BgImg />
       </Content>
     </Container>
   );
@@ -14,32 +17,40 @@ const Login = () => {
 const Container = styled.section`
   overflow: hidden;
   display: flex;
-  flex-direction:column;
-  text-align:center;
-  height:100vh;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
 `;
 const Content = styled.div`
-margin-bottom: 10vw;
-width: 100%;
-position: relative;
-min-height: 100vh;
-box-sizing: border-box;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-height: 100%;
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
 `;
 const BgImg = styled.div`
-height: 100%;
-background-position: top;
-background-size: cover;
-background-image: url("/images/login-background.jpg");
-background-repeat:no-repeat;
-position: absolute;
-top: 0;
-right: 0;
-left: 0;
-z-index: -1;
-`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-image: url("/images/login-background.jpg");
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+const CTA = styled.div``;
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
 export default Login;
