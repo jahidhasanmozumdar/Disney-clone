986609a8237bf87950f41905047b519ef3a9d825
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2023-01-28 00:22:56 +0600

    banner complete

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -6,7 +6,18 @@ const Login = () => {
     <Container>
       <Content>
         <CTA>
-          <CTALogoOne src="/images/rtaImage-removebg-preview.png" alt="logo" />
+          <CTALogoOne
+            src="/images/rtaImage-removebg-previe22w.png"
+            alt="logo"
+          />
+          <SingUp>Get All There</SingUp>
+          <Description>
+            {" "}
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="logo" />
         </CTA>
         <BgImg />
       </Content>
@@ -45,12 +56,48 @@ const BgImg = styled.div`
   left: 0;
   z-index: -1;
 `;
-const CTA = styled.div``;
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 const CTALogoOne = styled.img`
   margin-bottom: 12px;
   max-width: 600px;
   min-height: 1px;
   display: block;
+  width: 100%;
+`;
+const SingUp = styled.a`
+  font-size: 26px;
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 2.5px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3, 1);
+  font-size: 14px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
   width: 100%;
 `;
 export default Login;
